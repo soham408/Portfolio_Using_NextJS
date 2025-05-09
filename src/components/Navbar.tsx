bdd6f7c28b09cf2872ba933cd2067dc9f6cc2b83
soham408 <bambadesoham408@gmail.com>
2025-05-09 10:45:59 +0530

    09/05/25

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,8 @@ export default function NavbarComponent() {
                 S
               </div>
             </div> */}
-            <span className="text-xl font-bold tracking-wider text-gray-400">soham.dev /</span>
+            <span className="text-xl font-bold tracking-wider text-gray-400 uppercase
+            ">SOHAM ∞ /</span>
           </div>
           <nav className="">
             <ul className="flex space-x-8">
@@ -65,7 +66,7 @@ export default function NavbarComponent() {
            
           //  onClick={handleResumeDownload} 
            
-           variant="outline" className="bg-black-50 text-white">
+           variant="outline" className="bg-black-50 hover:bg-gray-500 text-white">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="24px" width="24px"><g strokeWidth="0" id="SVGRepo_bgCarrier"></g><g strokeLinejoin="round" strokeLinecap="round" id="SVGRepo_tracerCarrier"></g><g id="SVGRepo_iconCarrier"> <g id="Interface / Download"> <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" stroke="#f1f1f1" d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12" id="Vector"></path> </g> </g></svg>
                 Resume
               </Button>
